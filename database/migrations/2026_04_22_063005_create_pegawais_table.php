fcd98a096f5c4b488de7315a4ce7fb18f41118b7
@ridhoakbarsyah <ridhoramadhan096@gmail.com>
2026-04-23 17:40:40 +0700

    Update: hapus migrations yang tidak dipake

--- database/migrations/2026_04_22_063005_create_pegawais_table.php
+++ database/migrations/2026_04_22_063005_create_pegawais_table.php
@@ -6,38 +6,46 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('pegawais', function (Blueprint $table) 
         {
             $table->id();
-            $table->string('nip')->unique();
-            $table->string('nama');
-            $table->string('tempat_lahir');
+
+            $table->string('nip')->unique()->nullable();
+            $table->string('nama')->nullable();
+            $table->string('tempat_lahir')->nullable();
             $table->date('tgl_lahir');
             $table->enum('jenis_kelamin', ['L', 'P']);
-            $table->text('alamat');
+            $table->text('alamat')->nullable();
             $table->string('no_hp')->nullable();
             $table->string('npwp')->nullable();
             $table->string('foto')->nullable();
             $table->string('tempat_tugas')->nullable();
 
             $table->foreignId('agama_id')->constrained();
-            $table->foreignId('unit_kerja_id')->constrained();
-            $table->foreignId('jabatan_id')->constrained();
+
+            $table->foreignId('unit_kerja_id')
+                  ->nullable()
+                  ->constrained()
+                  ->nullOnDelete();
+
+            $table->foreignId('jabatan_id')
+                  ->nullable()
+                  ->constrained()
+                  ->nullOnDelete();
+
             $table->foreignId('golongan_id')->constrained();
-            $table->foreignId('eselon_id')->constrained();
+
+            $table->foreignId('eselon_id')
+                  ->nullable()
+                  ->constrained()
+                  ->nullOnDelete();
 
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('pegawais');
